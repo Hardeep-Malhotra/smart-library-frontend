74d6795bd53b1b77f6db8bc65e71087bf8c635bc
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-04-15 06:26:52 +0530

    feat: update Home component with Tailwind styles, add Navbar and Tailwind configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 
 
 export default function Home() {
-  return <h1>WelCome to Next.js </h1>
+  return   <h1 className="text-3xl text-red-500 font-bold">
+      Tailwind Working 🚀
+    </h1>
 }
---
+++ src/components/Nabnar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+
+const Navbar = ()=>{
+
+    return (
+        <nav>
+            <div>
+                <Link href={'/'}>
+                <div>
+                    <div>
+
+                    </div>
+                </div>
+                </Link>
+            </div>
+            <div>
+                <span></span>
+            </div>
+        </nav>
+    )
+}
+
+
+export default Navbar
