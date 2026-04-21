c8ae2ba0d0b6a9d60d81b2acb9faf5b5bc659feb
Hardeep-Malhotra <hardeepmalhotra022@gmail.com>
2026-04-21 16:15:46 +0530

    feat: add BookList component and integrate it into Home page; update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import Banner from "../components/Banner";
+import BookList from "../components/BookList";
 
-export default function Home() {
+export default async function Home() {
+  let book = [];
+
+  try {
+    const response = await fetch(`${process.env.BACKEND_URL}/api/books`);
+
+    console.log("Status Code:", response.status);
+
+    if (!response.ok) {
+      throw new Error(`Failed with status: ${response.status}`);
+    }
+
+    book = await response.json();
+
+    console.log(book);
+  } catch (error) {
+    console.log("Fetch Error:", error);
+  }
+
+  console.log(book);
+
   return (
     <>
       <Banner />
+      <BookList />
     </>
   );
 }
